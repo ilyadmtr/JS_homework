6469a36140a4fecd2477e239c38e7c0560ca414d
ilyadmtr <ilyadmytrenko@gmail.com>
2021-12-22 21:41:32 +0200

    added homewirk 3

---
+++ hw3/script.js
@@ -0,0 +1,33 @@
+//1
+// let a = prompt("Введите число a: ")
+// let b = prompt("Введите число b: ")
+// let x = (-b)/a
+
+// alert(`Результат х: ${x}`)
+
+// 2
+// let hours = prompt("Введите количество часов: ")
+// let minutes =  prompt("Введите количество минут: ")
+// const twHours = 24
+// const minutesInHours = 60
+
+// alert(`До завтрашнего дня осталось: ${twHours-hours} часов(а) и ${minutesInHours-minutes} минут(ы) `)
+
+// 3
+// let number = prompt("Введите трехзначное число: ");
+// number = number/10
+// let secondNumber = Math.floor(number%10)
+// alert(`Вторая цифра этого числа : ${secondNumber}`)
+
+// 4
+// let num = prompt("Введите пятизначное число: ");
+// let lastNum = String(Math.floor(num%10));
+// num = String(Math.floor(num/10));
+// alert(`Новое число : ${lastNum+num}`)
+
+// 5
+// let mainSalary  = 250;
+// let sale = prompt("Введите суму с продаж: ")
+// let allSalary = 250 + (sale/10);
+
+// alert(`Ваша зарплата в этом месяце: ${allSalary}`)
